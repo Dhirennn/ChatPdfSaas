bd872ba8af4466f4b0c20567d6593b32ad782ef4
Dhiren <mgur0007@student.monash.edu>
2024-12-18 15:15:32 +0800

    Add ChatToFilePage

---
+++ chat-with-pdf/app/dashboard/files/[id]/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ChatToFilePage(
+  {
+    params: { id }
+  } :
+  {
+    params: {
+      id: string
+    }
+  }
+
+) {
+  return (
+    <div>
+      ChatToFilePage { id }
+    </div>
+  )
+}
+
+export default ChatToFilePage
